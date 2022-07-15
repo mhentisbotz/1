e1d071d001cf81130c73596c42e5fd6ebb26dbfe
mhentisbotz <109332967+mhentisbotz@users.noreply.github.com>
2022-07-15 12:48:39 +0700

    Update and rename plugins/donasi.js to plugin/donasi.js

--- plugin/donasi.js
+++ plugin/donasi.js
@@ -3,11 +3,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ GoPay: 085828764046 [rzy-botz]
-├ Dana: 085828764046 [rzy-botz]
-├ Pulsa: 085828764046 [rzy-botz]
+├ Pulsa:  085646606905
+├ Dana: 085646606905
+├ Pulsa2: 081946945315
 └────
-`.trim(), watermark, 'Menu', '.menu', m)
+`.trim(), watermark, 'Back Raf Botz Menu', '.menu', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
